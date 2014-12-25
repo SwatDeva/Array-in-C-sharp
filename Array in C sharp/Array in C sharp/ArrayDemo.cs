608514af7dc2ac0eb5807065dc1d3bf2e30617a7
SwatDeva <shwethluvsall@gmail.com>
2014-12-25 12:11:05 +0000

    Initial Commit

---
+++ Array in C sharp/Array in C sharp/ArrayDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_in_C_sharp
+{
+    class ArrayDemo
+    {
+        public String[] getPandavasNames()
+        {
+            return new String[] {"Yudhistra","Bheema","Arjuna","Nakula","Sagadeva"};        
+        }
+    }
+}
---
+++ Array in C sharp/Array in C sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_in_C_sharp
+{
+    class Program
+    {
+        private String[] pandavas;
+        
+        static void Main(string[] args)
+        {
+            ArrayDemo arrayDemo = new ArrayDemo();
+            Program program = new Program();
+            program.pandavas = arrayDemo.getPandavasNames();
+            program.printPandavaNames();
+        }
+        
+        private void printPandavaNames()
+        {
+            for(int i =0;i<pandavas.Length;i++)
+            {
+                Console.WriteLine("Name "+i+": "+pandavas[i]);
+            }
+        }
+    }
+}
